9864877501327193bddb06bcbe7687ae2f9f51d8
LaitMax <maksim.slobodchikov21@gmail.com>
2023-10-03 22:12:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Process.Start("notepad.exe");
+        Console.WriteLine("Hello, World!");
+      //  string text = "HelloWorld!";
+       // string writePath = "C:/Users/User/Desktop/hello.txt";
+        using (StreamWriter sw = new StreamWriter("C:/Users/User/Desktop/hello.txt"))
+        {
+            sw.WriteLine("Hello Pavel Olegovich");
+        }
+    }
+}
